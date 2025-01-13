c0a86ca6a6298522446466fa03fe2125840c0022
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-01-13 12:23:34 +0300

    Final

---
+++ MyFinancialCRM/FrmLogin.cs
@@ -0,0 +1,40 @@
+﻿using MyFinancialCRM.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFinancialCRM
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+        DBFinancialCRMEntities db = new DBFinancialCRMEntities();
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtUserId.Text);
+            string password = txtPassword.Text;
+            var users = db.Users.Find(id);
+                if (users.UserId == id && users.Password == password)
+                {
+                    FrmDashboard frm = new FrmDashboard();
+                    frm.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Kullanıcı ID veya Şifre hatalı!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        
+    }
+}
--- MyFinancialCRM/Program.cs
+++ MyFinancialCRM/Program.cs
@@ -16,7 +16,7 @@ namespace MyFinancialCRM
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmBanks());
+            Application.Run(new FrmLogin());
         }
     }
 }
